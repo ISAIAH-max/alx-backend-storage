62a262686f071f2f9edd665c3ead7359ca82d1e8
ISAIAH-max <mlewaisaiah@gmail.com>
2021-09-23 01:46:57 +0300

    Enumeration used in the script

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Creates a table users
-CREATE table IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
