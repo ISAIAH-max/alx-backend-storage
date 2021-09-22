b7a0caf76e3c3b845abd7b05a4878f53f25c0572
ISAIAH-max <mlewaisaiah@gmail.com>
2021-09-23 01:23:41 +0300

    A SQL script that creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- creates a table users
 CREATE table IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email  VARCHAT(255) NOT NULL UNIQUE,
+    email  VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
