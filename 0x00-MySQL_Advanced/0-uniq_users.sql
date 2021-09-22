eaf1aae9b588f2838ac8cb8bda5713f71d953146
ISAIAH-max <mlewaisaiah@gmail.com>
2021-09-23 02:26:46 +0300

    creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- creates a table users
-CREATE table IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email  VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
